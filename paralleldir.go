a1353eb5510da238be0ec7339be46a7c962ed1cb
ricardoitv <120180055+ricardoitv@users.noreply.github.com>
2025-12-23 18:41:13 +0000

    WIP: Paralellising with go routines and WaitGroups

--- paralleldir.go
+++ paralleldir.go
@@ -4,36 +4,55 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 )
 
 type ParallelDir struct {
-	baseDir string
+	root *treeNode
+}
+
+type treeNode struct {
+	name     string
+	children []*treeNode
 }
 
 func NewParallelDir(baseDir string) *ParallelDir {
-	return &ParallelDir{baseDir: baseDir}
-}
-
-func (pd *ParallelDir) Run() {
-	fmt.Println("Starting on", pd.baseDir)
-	for _, dir := range pd.listDirs() {
-		fmt.Println("-", dir)
+	return &ParallelDir{
+		root: &treeNode{
+			name:     baseDir,
+			children: make([]*treeNode, 0),
+		},
 	}
 }
 
-// listDirs Returns a list of directories inside of the current base directory
-func (pd *ParallelDir) listDirs() []string {
-	files, err := os.ReadDir(pd.baseDir)
+func (pd *ParallelDir) baseDir() string {
+	return pd.root.name
+}
+
+func (pd *ParallelDir) Run() {
+	var wg sync.WaitGroup
+	fmt.Println("Starting on", pd.baseDir())
+	listDirsRecursively(pd.root, &wg)
+	wg.Wait()
+	fmt.Println(pd.root)
+}
+
+func listDirsRecursively(node *treeNode, wg *sync.WaitGroup) {
+	files, err := os.ReadDir(node.name)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	dirs := make([]string, 0)
 	for _, file := range files {
 		if file.IsDir() {
-			dirs = append(dirs, file.Name())
+			childNode := &treeNode{
+				name:     file.Name(),
+				children: make([]*treeNode, 0),
+			}
+			node.children = append(node.children, childNode)
+			wg.Go(func() {
+				listDirsRecursively(childNode, wg)
+			})
 		}
 	}
-
-	return dirs
 }

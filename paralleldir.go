1d774d9733d56b76344de2eaffb52939d1812ac8
ricardoitv <120180055+ricardoitv@users.noreply.github.com>
2025-12-23 21:56:57 +0000

    Reordering code

--- paralleldir.go
+++ paralleldir.go
@@ -10,43 +10,14 @@ import (
 	"sync"
 )
 
-type ParallelDir struct {
-	Root *treeNode `json:"root"`
-}
-
 type treeNode struct {
 	Name     string      `json:"name"`
 	Children []*treeNode `json:"children"`
 	mu       sync.Mutex
 }
 
-func (pd *ParallelDir) ToJson() string {
-	bytes, err := json.Marshal(pd.Root)
-	if err != nil {
-		log.Fatal("Error converting final tree to JSON:", err.Error())
-	}
-	return string(bytes)
-}
-
-func (t *treeNode) SafeAppendChild(childNode *treeNode) {
-	t.mu.Lock()
-	defer t.mu.Unlock()
-	t.Children = append(t.Children, childNode)
-}
-
-func (t *treeNode) ToString() string {
-	return t.toStringWithIndent("")
-}
-
-func (t *treeNode) toStringWithIndent(indent string) string {
-	var result strings.Builder
-
-	result.WriteString(indent + t.Name + "\n")
-	for _, child := range t.Children {
-		result.WriteString(child.toStringWithIndent(indent + "\t"))
-	}
-
-	return result.String()
+type ParallelDir struct {
+	Root *treeNode `json:"root"`
 }
 
 func NewParallelDir(baseDir string) *ParallelDir {
@@ -67,8 +38,38 @@ func (pd *ParallelDir) Run() {
 	fmt.Println("Starting on", pd.baseDir())
 	pd.Root.listDirsRecursively(&wg)
 	wg.Wait()
-	//fmt.Println(pd.Root.ToString())
-	fmt.Println(pd.ToJson())
+	fmt.Println(pd.Root.toString())
+	fmt.Println(strings.Repeat("-", 100))
+	fmt.Println(pd.toJson())
+}
+
+func (pd *ParallelDir) toJson() string {
+	bytes, err := json.Marshal(pd.Root)
+	if err != nil {
+		log.Fatal("Error converting final tree to JSON:", err.Error())
+	}
+	return string(bytes)
+}
+
+func (t *treeNode) safeAppendChild(childNode *treeNode) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.Children = append(t.Children, childNode)
+}
+
+func (t *treeNode) toString() string {
+	return t.toStringWithIndent("")
+}
+
+func (t *treeNode) toStringWithIndent(indent string) string {
+	var result strings.Builder
+
+	result.WriteString(indent + t.Name + "\n")
+	for _, child := range t.Children {
+		result.WriteString(child.toStringWithIndent(indent + "\t"))
+	}
+
+	return result.String()
 }
 
 func (t *treeNode) listDirsRecursively(wg *sync.WaitGroup) {
@@ -82,7 +83,7 @@ func (t *treeNode) listDirsRecursively(wg *sync.WaitGroup) {
 				Name:     filepath.Join(t.Name, file.Name()),
 				Children: make([]*treeNode, 0),
 			}
-			t.SafeAppendChild(childNode)
+			t.safeAppendChild(childNode)
 			wg.Go(func() {
 				childNode.listDirsRecursively(wg)
 			})

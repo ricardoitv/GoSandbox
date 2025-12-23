968b9e4612ee8ea076d6aa6c574dec18630742de
ricardoitv <120180055+ricardoitv@users.noreply.github.com>
2025-12-23 15:38:23 +0000

    Listing the directories on a given base directory

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
 func main() {
-	pd := NewParallelDir("/Users/ricaamar/Code/Personal/")
+	pd := NewParallelDir("/Users/ricaamar/Code/")
 	pd.Run()
 }
--- paralleldir.go
+++ paralleldir.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 type ParallelDir struct {
 	baseDir string
@@ -12,4 +16,24 @@ func NewParallelDir(baseDir string) *ParallelDir {
 
 func (pd *ParallelDir) Run() {
 	fmt.Println("Starting on", pd.baseDir)
+	for _, dir := range pd.listDirs() {
+		fmt.Println("-", dir)
+	}
+}
+
+// listDirs Returns a list of directories inside of the current base directory
+func (pd *ParallelDir) listDirs() []string {
+	files, err := os.ReadDir(pd.baseDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dirs := make([]string, 0)
+	for _, file := range files {
+		if file.IsDir() {
+			dirs = append(dirs, file.Name())
+		}
+	}
+
+	return dirs
 }
